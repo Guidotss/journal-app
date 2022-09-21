1bb1ca4c2e3e4f08b5ae2ef1400bca455bad144c
Guidotss <100441914+Guidotss@users.noreply.github.com>
2022-09-21 19:52:11 -0300

    crando Nothing selected view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,14 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
 import { JournalLayOut } from "../layout/JournalLayOut"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <>
       <JournalLayOut>
-        <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui itaque impedit quae possimus ab voluptas dicta suscipit provident a, facilis ullam, voluptatibus incidunt unde. Nobis velit iure architecto facere vero.</Typography>
+        {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui itaque impedit quae possimus ab voluptas dicta suscipit provident a, facilis ullam, voluptatibus incidunt unde. Nobis velit iure architecto facere vero.</Typography> */}
+        <NothingSelectedView/>
       </JournalLayOut>
     </>
   )
