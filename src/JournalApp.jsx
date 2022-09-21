c49f0af1dbbfc17eafd82338a97dab46512d495b
Your Name <you@example.com>
2022-09-21 14:39:46 -0300

    agregando materialUI

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,7 +1,13 @@
+import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme/AppTheme"
+
+
 export const JournalApp = () => {
   return (
     <>
-        <h1>hola mundo</h1>
+        <AppTheme>
+          <AppRouter/>
+        </AppTheme>
     </>
   )
 }
