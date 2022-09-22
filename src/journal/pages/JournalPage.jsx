3d054255519ceb8b64864038e0836ffa15d61214
Your Name <you@example.com>
2022-09-22 11:48:09 -0300

    agregando boton flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { AddOutlined, MailOutline } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayOut } from "../layout/JournalLayOut"
 import { NoteView, NothingSelectedView } from "../views"
 
@@ -8,9 +8,13 @@ export const JournalPage = () => {
     <>
       <JournalLayOut>
         {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui itaque impedit quae possimus ab voluptas dicta suscipit provident a, facilis ullam, voluptatibus incidunt unde. Nobis velit iure architecto facere vero.</Typography> */}
-        {/* <NothingSelectedView/> */}
-        <NoteView/>
+        <NothingSelectedView/>
+        {/* <NoteView/> */}
       </JournalLayOut>
+
+      <IconButton size='large' sx={{color:'white',backgroundColor:'error.main',':hover':{backgroundColor:'error.main',opacity:0.9},position:'fixed',right:50,bottom:50}}>
+        <AddOutlined sx={{fontSize:30}}/>
+      </IconButton>
     </>
   )
 }
